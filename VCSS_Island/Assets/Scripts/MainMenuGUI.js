44157dbcba956b84176cb52b58c38fa8e30103ef
CptnSlaughter <kate.r.slaughter@gmail.com>
2014-11-06 16:59:37 +0000

    Menu added. Also made a fix to the crosshair (as per Keenan's request)

---
+++ VCSS_Island/Assets/Scripts/MainMenuGUI.js
@@ -0,0 +1,53 @@
+﻿#pragma strict
+
+var beep : AudioClip;
+var menuSkin : GUISkin;
+var menuArea : Rect;
+var playButton : Rect;
+var instructionsButton : Rect;
+var quitButton : Rect;
+var instructions : Rect;
+private var menuAreaNormalized : Rect;
+private var menuPage : String = "main";
+
+function Start(){
+	menuAreaNormalized = Rect(menuArea.x * Screen.width - (menuArea.width * 0.5f), menuArea.y * Screen.height - (menuArea.height * 0.5f), menuArea.width, menuArea.height);
+}
+
+function OnGUI(){
+	GUI.skin = menuSkin;
+	GUI.BeginGroup(menuAreaNormalized);
+	if(menuPage == "main"){
+		if(GUI.Button(Rect(playButton), "Play")){
+			ButtonAction("Island");
+		}
+		if(GUI.Button(Rect(instructionsButton), "Instructions")){
+			audio.PlayOneShot(beep);
+			menuPage = "instructions";
+		}
+		if(GUI.Button(Rect(quitButton), "Quit")){
+			ButtonAction("quit");
+		}
+	}
+	else if(menuPage == "instructions"){
+		GUI.Label(Rect(instructions), "You awake on a mysterious island... Find a way to signal for help or face certain DOOM!");
+		if(GUI.Button(Rect(quitButton),"Back")){
+			audio.PlayOneShot(beep);
+			menuPage="main";
+		}
+	}
+	GUI.EndGroup();
+}
+
+function ButtonAction(levelName : String){
+	audio.PlayOneShot(beep);
+	yield new WaitForSeconds(0.35f);
+	if(levelName != "quit"){
+		Application.LoadLevel(levelName);
+	}else{
+		Application.Quit();
+		Debug.Log("Have Quit");
+	}
+}
+
+@script RequireComponent(AudioSource)
