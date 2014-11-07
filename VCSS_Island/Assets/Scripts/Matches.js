caa5178d9b5b087df199d66fb4b3b59e2b01e694
pavlov2000uk <pavlov2000uk@gmail.com>
2014-11-07 02:45:14 +0200

    Outpost, Generator and Powercells

---
+++ VCSS_Island/Assets/Scripts/Matches.js
@@ -0,0 +1,8 @@
+﻿function OnTriggerEnter(col : Collider)
+	{
+		if(col.gameObject.tag == "Player")
+			{
+				col.gameObject.SendMessage("MatchPickup");
+				Destroy(gameObject);
+			}
+	}
--- VCSS_Island/Assets/Scripts/PowerCell.js
+++ VCSS_Island/Assets/Scripts/PowerCell.js
@@ -2,20 +2,17 @@
 
 var rotationSpeed : float = 100.0;
 
-function Start () {
-
-}
-
-function Update () {
-	
-	transform.Rotate(Vector3(0,rotationSpeed * Time.deltaTime,0));
-
-}
-
-function OnTriggerEnter(col : Collider){
-	if(col.gameObject.tag == "Player"){
-		col.gameObject.SendMessage("CellPickup");
-		Destroy(gameObject);
+function Update () 
+	{
+		transform.Rotate(Vector3(0,rotationSpeed * Time.deltaTime,0));
 	}
-}
+
+function OnTriggerEnter(col : Collider)
+	{
+		if(col.gameObject.tag == "Player")
+			{
+				col.gameObject.SendMessage("CellPickup");
+				Destroy(gameObject);
+			}
+	}
 
